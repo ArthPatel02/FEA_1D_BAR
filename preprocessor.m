4eeb37cd8b6fa446c998de2456bf33b895a687eb
ArthPatel02 <iarthpatel1405@gmail.com>
2025-03-09 11:35:28 +0530

    PreP - Added Domain of Bar

---
+++ preprocessor.m
@@ -0,0 +1,6 @@
+% Prompt user for input of two variables
+a = input('Enter the Domain of bar (Left) : ');
+b = input('Enter the Domain of bar (Right): ');
+
+% Display the entered values
+fprintf('Domain : (%g,%g)\n', a, b);
