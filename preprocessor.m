24b06277a6a25ae3d708843584493b86f3e2cb09
ArthPatel02 <iarthpatel1405@gmail.com>
2025-03-12 22:26:08 +0530

    Defined Sections

--- preprocessor.m
+++ preprocessor.m
@@ -3,4 +3,28 @@ a = input('Enter the Domain of bar (Left) : ');
 b = input('Enter the Domain of bar (Right): ');
 
 % Display the entered values
-fprintf('Domain : (%g,%g)\n', a, b);
+ fprintf('Domain : (%g,%g)\n', a, b);
+
+% Read the number of input points
+totalNode = input('Enter the total number of nodes : ');
+
+% Initialize a row vector for x-coordinates
+nodeArray = zeros(1, totalNode);
+
+disp('Enter the node location :');
+for i = 1:totalNode
+nodeArray(i) = input(sprintf('Node(%d): ', i));
+end
+
+% Display the Node Array
+disp('Entered Nodes are :');
+disp(nodeArray);
+
+% Define Section
+secLength = zeros(1, totalNode-1);
+
+for i = 1:(totalNode-1)
+    secLength(i) = nodeArray(i+1) - nodeArray(i);
+end
+disp('Section Length are : :');
+disp(secLength);
