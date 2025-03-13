72313313b43fbefd41f4c6e4faf54941fd76b918
Naveenkumar026 <naveenkumar547678@gmail.com>
2025-03-13 23:34:19 +0530

    just checking

--- postprocessor.m
+++ postprocessor.m
@@ -1,1 +1,5 @@
-
+// Graph of nodal displacement along  with  coordinates
+// Co = coordinates , ND = nodeal displacement
+ND = [1,2,3,4,5,6,7,8,9,10]
+Co = [1.5,2.5,3.5,4,5.5,6.6,7,8,9,9.5]
+plot(Co,ND,"-Ro")
