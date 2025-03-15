9ba8c8b6bcc86878673dc8c9e55fde582ad2dcee
sumitsingh264 <ce22b034@iittp.ac.in>
2025-03-15 18:27:35 +0530

    Added function for internal force

--- processor.m
+++ processor.m
@@ -15,7 +15,7 @@ function K = globalStiffnessMatrix(n)
 K = zeros(n,n);
 ke = elementStiffnessMatrix(E,A,L);
 for i= 1:n
-K(i:i+1,i:i+1) = k(i:i+1,i:i+1) + ke;
+K(i:i+1,i:i+1) = K(i:i+1,i:i+1) + ke;
 end 
 
 % function for Shape Function and its derivatives 
@@ -34,3 +34,33 @@ function [N1,N2,D1,D2] = shapeFunction(X)
     D2(i) = diff(N2(i),x);
     end
      
+    % function for internal and external force
+    function [bi,b1,b2,F] = forceFunction(q,R,n)
+        [N1,N2,D1,D2] = shapeFunction([0 2 4 6]);
+        X = [0 2 4 6];
+        L = 6;
+        numberofNodes = n;
+                syms U1 U2 x
+                U = N1*U1 + N2*U2;
+                b1 = sym(zeros(numberofNodes-1,1));
+                b2 = sym(zeros(numberofNodes-1,1));
+                F = sym(zeros(numberofNodes-1,1));
+        
+        
+              for j = 1:numberofNodes-1
+                    f1(x) = N1(j)*R;
+                    g1(x) = N1(j)*q;
+                      f2(x) = N2(j)*R;
+                    g2(x) = N2(j)*q;
+        
+                    b1(j) = int(g1,x,X(j),X(j+1)) + f1(X(4));
+                    b2(j) = int(g2,x,X(j),X(j+1)) + f2(X(4));
+                    
+              end
+              b1_padded = [b1;0];
+              b2_padded = [0;b2];
+              bi = zeros(numberofNodes,1);
+              bi = b1_padded + b2_padded ;
+              
+        end
+        
