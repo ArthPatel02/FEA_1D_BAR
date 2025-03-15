c70d1c4889316e2bc5148d0680c3aa92c0454b4c
sumitsingh264 <ce22b034@iittp.ac.in>
2025-03-15 13:38:39 +0530

    Added Function Shape function

--- processor.m
+++ processor.m
@@ -17,4 +17,20 @@ ke = elementStiffnessMatrix(E,A,L);
 for i= 1:n
 K(i:i+1,i:i+1) = k(i:i+1,i:i+1) + ke;
 end 
-kim 
+
+% function for Shape Function and its derivatives 
+function [N1,N2,D1,D2] = shapeFunction(X)
+    syms x
+    numberofNodes = numel(X);
+    N1 = sym(zeros(1,numberofNodes-1));
+    N2 = sym(zeros(1,numberofNodes-1));
+    D1 = sym(zeros(1,numberofNodes-1));
+    D2 = sym(zeros(1,numberofNodes-1));
+    
+    for i = 1:numberofNodes-1
+    N1(i) = (X(i+1)-x)/(X(i+1)-X(i));
+    N2(i) = (x-X(i))/(X(i+1)-X(i));
+    D1(i) = diff(N1(i),x);
+    D2(i) = diff(N2(i),x);
+    end
+     
